7e7d20a4b75c9671a62e99aa0aafd3ece103d94d
joeys1122 <josephs1122@gmail.com>
2022-02-07 14:33:45 -0500

    added GET enpoints

--- api/server.js
+++ api/server.js
@@ -1,3 +1,27 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const usersModel = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.get('/api/users', (req, res) => {
+  usersModel.find()
+    .then(user => {
+      res.json(user);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "The users information could not be retrieved" });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  usersModel.findById(req.params.id)
+    .then(user => {
+      user ? res.json(user) : res.status(404).json({ message: "The user with the specified ID does not exist" })
+    })
+    .catch(() => {
+      res.status(404).json({ message: "The user with the specified ID does not exist" });
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
